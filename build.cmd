e3701ce6977bc2c72183de7e72f84212f79838dd
adontz <adontz@gmail.com>
2020-08-15 03:06:56 +0400

    Add FreeFind search.

--- build.cmd
+++
@@ -1,6 +0,0 @@
-CMD /C antora generate --clean playbook.yml
-
-COPY /y index.html .\..\nt-rest.github.io\docs\
-COPY /y CNAME      .\..\nt-rest.github.io\docs\
-COPY /y NUL        .\..\nt-rest.github.io\docs\.nojekyll
-
